83ba0d04ea5676d5c9c07f906573e6e242c32296
JacobGo <jacobmgoldman@icloud.com>
2018-03-05 23:01:17 +0000

    Added logging

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
 from flask import Flask, request
+import logging
+from datetime import datetime
  
+logging.basicConfig(filename="/var/www/bigapp/logs/" + datetime.now().strftime('bigapp_%H_%M_%d_%m_%Y.log'), level=logging.DEBUG)
 app = Flask(__name__)
  
-@app.route('/', methods=['GET'])
+@app.before_request
+def log_request_info():
+    logging.debug('Headers: %s', request.headers)
+    logging.debug('Body: %s', request.get_data())
+
+@app.route('/verify', methods=['GET'])
 def handle_verification():
     return request.args['hub.challenge']
- 
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'Testing!!!!!' 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=True, port=3000)
